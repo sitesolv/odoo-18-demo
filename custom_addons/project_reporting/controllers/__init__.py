8ded9153e9a770dc93283bb62722ec4dfc990167
Your Name <premjalith@gmail.com>
2025-10-29 09:54:06 +0530

    gggg

--- custom_addons/project_reporting/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import dashboard
-from . import dashboard_controller
-from . import test_controller
-from . import data_initializer
--- custom_addons/project_reporting/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import project_dashboard
-from . import project_reporting_view
-from . import project_extended
